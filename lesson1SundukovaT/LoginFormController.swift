585b614d1f69fbd959d936eb8b44d624047c2fc5
Tatiana <stanya21@yandex.ru>
2020-12-03 19:43:41 +0300

    Lesson 1

---
+++ lesson1SundukovaT/LoginFormController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginFormController.swift
+//  lesson1SundukovaT
+//
+//  Created by Татьяна Сундукова on 03.12.2020.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var loginLabel: UILabel!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBAction func loginField(_ sender: UITextField) {
+    }
+    
+    @IBAction func passwordField(_ sender: UITextField) {
+    }
+    
+    @IBAction func buttonEnter(_ sender: Any) {
+    }
+    
+}
