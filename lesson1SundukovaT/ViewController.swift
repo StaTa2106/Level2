bed5b2161ea5f27336a04f5d5976c3e5b35be422
Tatiana <stanya21@yandex.ru>
2020-12-03 13:39:56 +0300

    Initial Commit

---
+++ lesson1SundukovaT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1SundukovaT
+//
+//  Created by Татьяна Сундукова on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
